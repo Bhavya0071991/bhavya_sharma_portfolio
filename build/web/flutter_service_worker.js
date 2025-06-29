d12cfa16f62a6faeab1f76119361ea12a651ebca
Bhavya0071991 <freckey@gmail.com>
2025-06-29 20:04:13 +0530

    initial commit

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "8fafa5395b0f8abf8f84a323bf68217e
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "b65f5140e398485d8b2a6f7afb4070b6",
+"flutter_bootstrap.js": "3f20945a974a7ecbdf0fb2fb81d75deb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
